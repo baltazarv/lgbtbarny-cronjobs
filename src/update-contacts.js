fd6df7d91bebe8bd81ddce470362f2d2f6573bd9
Baltazar Villegas <baltazar@xogene.com>
2021-06-04 16:09:01 -0400

    Test again with time diff

--- src/update-contacts.js
+++ src/update-contacts.js
@@ -176,7 +176,7 @@ const updateContacts = async () => {
         console.log('Total records:', totalRecords);
         console.log('Updated records:', countUpdatedRecs);
         console.log('Created records:', countCreatedRecs);
-        console.log('Started:', timeDateStarted, '; ended', moment());
+        console.log('Start:', timeDateStarted, ': End', moment(), ': Diff', timeDateStarted.diff(moment(), 'minutes'));
       }
     )
 }
