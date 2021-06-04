238712544237dcfc26ed0c22775235ce94cf31f6
Baltazar Villegas <baltazar@xogene.com>
2021-06-04 14:35:41 -0400

    Test access to Airtable
    Console logging groups.

--- src/update-contacts.js
+++ src/update-contacts.js
@@ -1,4 +1,4 @@
-const sibUtils = require('../libs/sendinblue');
+const sibUtils = require('./libs/sendinblue');
 const constants = require('./constants');
 const moment = require('moment');
 
@@ -6,13 +6,21 @@ const sibContactsApi = sibUtils.sibContactsApi;
 // constants
 const sibFields = constants.sibFields;
 const sibLists = constants.sibLists;
-// airtable constants
+
+// airtable
+const airtableUtils = require("./libs/Airtable");
+// const getMinifiedRecord = airtableUtils.getMinifiedRecord;
+const getGroups = airtableUtils.getGroups;
+// constants
 const dbFields = constants.dbFields;
 
 const updateContacts = async () => {
   console.log('Hello contacts!');
   console.log('env var', process.env.TEST);
   console.log('logging env vars above?');
+
+  const groups = await getGroups();
+  console.log('GROUPS', groups);
 
   let contactsApi = new sibContactsApi();
 }
